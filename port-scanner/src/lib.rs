0fa7b869fe2928fc9e02ef6accc9ef792f4b853c
lambertsbennett <lambertsbennett@gmail.com>
2024-02-21 22:12:21 +0100

    functional scanner

--- port-scanner/src/lib.rs
+++ port-scanner/src/lib.rs
@@ -1,12 +1,35 @@
 use tokio::net::TcpStream;
 use std::error::Error;
+use std::fmt;
 
+#[derive(Debug)]
+pub struct PingError {
+    pub message: String,
+}
 
-pub async fn check_port(query: &str) -> Result<(), Box<dyn Error>>{
+impl fmt::Display for PingError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl Error for PingError {}
+
+pub async fn ping_host(host: &str) -> Result<(), Box<dyn Error>>{
+    let stream = TcpStream::connect(host).await?;
+    match stream.try_write(&[1]) {
+        Ok(_n) => {},
+        Err(_) => return Err(Box::new(PingError{message: String::from("Error writing to TCPStream!")}))
+    }
+    match stream.try_read(&mut [0; 128]){
+        Ok(_n) => {},
+        Err(_) => return Err(Box::new(PingError{message: String::from("Error reading from TCPStream!")}))
+    }
+    Ok(())
+}
+
+pub async fn check_port(query: &str) {
     if let Ok(_stream) = TcpStream::connect(query).await {
         println!("{query} is open!");
-        Ok(())
-    } else {
-        Ok(())
-    }
+    } else {}
 }
--- port-scanner/src/main.rs
+++ port-scanner/src/main.rs
@@ -1,7 +1,9 @@
-use port_scanner::check_port;
+use port_scanner::{check_port, ping_host};
 use std::fmt;
 use std::env;
 use std::error::Error;
+
+type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 #[derive(Debug)]
 pub struct ArgParseError {
@@ -10,24 +12,22 @@ pub struct ArgParseError {
 
 impl  fmt::Display for ArgParseError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.message)
+        write!(f, "{0}", self.message)
     }
 }
 
 impl Error for ArgParseError {}
 
 #[tokio::main]
-async fn main() -> Result<(), ArgParseError> {
+async fn main() -> Result<()> {
 
-    let target = env::args().nth(1);
-    match target {
-        Some(target) =>  {
-            for p in 1..65536 {
-                let query: String = [&target, ":", &p.to_string()].join("");
-                tokio::spawn( async move {let _ = check_port(&query).await;} );
-            }
-            Ok(())
-        },
-        None => Err(ArgParseError{message: String::from("No Arguments Provided!")})
+    let target = env::args().nth(1).ok_or(ArgParseError{message: String::from("No Arguments Provided!")})?;
+    let ping_target = target.clone();
+    let handle = tokio::spawn( async move { let _ = ping_host(&ping_target).await; } );
+    let _out = handle.await?;
+    for p in 1..65536 {
+        let query: String = [&target, ":", &p.to_string()].join("");
+        tokio::spawn( async move { check_port(&query).await; } );
     }
+    Ok(())
 }
