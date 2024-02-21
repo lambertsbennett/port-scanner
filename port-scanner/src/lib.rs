be851a593450150b9cb5bab87b9d3c1484e1ea25
lambertsbennett <lambertsbennett@gmail.com>
2024-02-21 19:09:02 +0100

    impl ArgParseError

--- port-scanner/src/lib.rs
+++ port-scanner/src/lib.rs
@@ -1,17 +1,12 @@
 use tokio::net::TcpStream;
 use std::error::Error;
 
-#[derive(Debug)]
-pub enum PortResult {
-    Open,
-    Closed
-}
 
-pub async fn check_port(query: &str) -> Result<PortResult, Box<dyn Error>>{
+pub async fn check_port(query: &str) -> Result<(), Box<dyn Error>>{
     if let Ok(_stream) = TcpStream::connect(query).await {
         println!("{query} is open!");
-        Ok(PortResult::Open)
+        Ok(())
     } else {
-        Ok(PortResult::Closed)
+        Ok(())
     }
 }
--- port-scanner/src/main.rs
+++ port-scanner/src/main.rs
@@ -1,14 +1,33 @@
-use port_scanner::{check_port, PortResult};
+use port_scanner::check_port;
+use std::fmt;
+use std::env;
 use std::error::Error;
 
+#[derive(Debug)]
+pub struct ArgParseError {
+    pub message: String,
+}
+
+impl  fmt::Display for ArgParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl Error for ArgParseError {}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<(), ArgParseError> {
 
-    let host: &str = "127.0.0.1";
-    for p in 1..65536 {
-        let query: String = [&host, ":", &p.to_string()].join("");
-        tokio::spawn( async move {let _ = check_port(&query).await;} );
+    let target = env::args().nth(1);
+    match target {
+        Some(target) =>  {
+            for p in 1..65536 {
+                let query: String = [&target, ":", &p.to_string()].join("");
+                tokio::spawn( async move {let _ = check_port(&query).await;} );
+            }
+            Ok(())
+        },
+        None => Err(ArgParseError{message: String::from("No Arguments Provided!")})
     }
-    Ok(())
 }
